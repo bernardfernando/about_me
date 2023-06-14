a3ab7f7799652d10b5ce0017969713c7c64bf62f
bernardfernando <jagathf@gmail.com>
2023-06-14 19:50:09 +0100

    amended

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # about_me
+
+13 opps - what a day, I had to persevere
+
+
+14 June 2023
+
+# Feature tasks
+HTML
+1. ## Do an ordered list eg Food I like, Countried I like to visit
+2. ## Do an unOrdered list for work experience and education summary
+
+Javascript
+1. Add 6th question to the guessing game - use the previous one to do this.
+2. Add a 7th question to the same
+
--- add_code.js
+++ add_code.js
@@ -40,7 +40,7 @@ if (answer === "no") {
 function quiz2() {
     let answer2;
         while(answer2 != "yes" && answer2 !="No" && answer2 != "y" &&  answer2 != "no") {
-            answer2  = prompt("Do you know my where I live, unless I get a correct answer you are asked to do that - lol") .toLowerCase(); 
+            answer2  = prompt("Do you know where I live, unless I get a correct answer you are asked to do that - lol") .toLowerCase(); 
             console.log(answer2);
     }
    
@@ -72,7 +72,7 @@ function quiz3() {
 
 if(answer3 ==="yes" || answer3 ==="y") {
 
-   let  myVillage = prompt("what is the name of the village I live in  <please click OK after entering your answer>").toLowerCase();
+   let  myVillage = prompt("What is the name of the village I live in  <please click OK after entering your answer>").toLowerCase();
     console.log(myVillage);
     if(myVillage ==="cawston" ){
         alert("Yes the village I live in is correct, weldone");
@@ -89,7 +89,7 @@ if (answer3 === "no") {
 /* Quiz 4 */
 function quiz4() {
     let answer4;
-        while(answer4 != "yes" && answer4 !="No" && answer4 != "y" &&  answer4 != "no") {
+        while(answer4 !="yes" && answer4 !="No" && answer4 !="y" &&  answer4 !="no") {
             answer4  = prompt("Do you know the music instrument I am learning, I want an acceptable answer - lol") .toLowerCase(); 
             console.log(answer4);
     }
@@ -97,7 +97,7 @@ function quiz4() {
 
 if(answer4 ==="yes" || answer4 ==="y") {
 
-   let  myInstrument = prompt("what is the instrument I am trying to learn  <please click OK after entering your answer>").toLowerCase();
+   let  myInstrument = prompt("What is the instrument I am trying to learn  <please click OK after entering your answer>").toLowerCase();
     console.log(myInstrument);
     if(myInstrument ==="guitar" ){
         alert("Yes the instrument I learn is the guitar, your answer is correct, weldone");
@@ -110,4 +110,56 @@ if (answer4 === "no") {
 
 }
 
+/* Quiz from 14 June 2023  Guess my number*/
+
+/* Pudo code  */
+// assign a numeric variable
+// do alert to get the feed from user
+/*
+function quiz5() {
+    let i=0;
+    let myNumber = 20;
+    let guessMyNumber = prompt("Guess my number");
+    for(i = 0; i < 4 ; i++) {
+        console.log(i);
+        if (myNumber === guessMyNumber) {
+            alert("Well done my number is 20");
+            break;
+            } else if(myNumber > gussMyNumber) {
+                alert("Your guess is  too small ");
+                guessMyNumber = prompt("ENTER IT AGAIN you are allowed "+ (4-i) + " more chances");
+                }   else if(myNumber < guessMyNumber) {
+                    alert("your guess is too big");
+                    guessMyNumber = prompt("ENTER IT AGAIN you are allowed "+ (4-i) + "more time");
+                    }
+                       
+        
+            console.log(guessMyNumber); 
+    
+    
+    }
+    
+} 
+*/
+
+function quiz5() {
+    let i = 0;
+    let myNumber = 20;
+
+    let guessMyNumber = prompt("Guess my number - you have 5 turns in total");
+    for(i = 0; i < 5 ; i++) {
+        console.log(i);
+        if (myNumber == guessNumber) {
+            alert("Well done you guessed it correct, efforts " + i);
+            break;
+        } else if ( myNumber > guessMyNumber ) { 
+            alert("your number is too small");
+            guessMyNumber = prompt("Give it another go");
+            } else 
+            alert("Gussed number is too small");
+            guessMyNumber = prompt("Please re-enter the number")       
+
+        }
+
+    }
 
